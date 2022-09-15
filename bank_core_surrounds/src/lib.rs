83daf667ccc9d7e5e1f4b92435230a3cec5c2253
Michael Chermside <mcherm@mcherm.com>
2022-09-14 21:45:17 -0400

    Finished splitting it into a library and a separate WASM app that uses the library.

--- bank_core_surrounds/src/lib.rs
+++ bank_core_surrounds/src/lib.rs
@@ -12,6 +12,7 @@ use std::sync::{Mutex, Once};
 use std::borrow::BorrowMut;
 use document::TwoTreeViewDocument;
 use capability_tree::read_csv_from_str;
+use prog_draw::text_size;
 use wasm_bindgen::prelude::*;
 
 
@@ -27,40 +28,39 @@ extern {
 pub struct FontError;
 
 
-// struct WASMTextSizer;
+struct WASMTextSizer;
 
-// impl text_size::TextSizer for WASMTextSizer {
-//     fn text_size(&self, text: &str, font_family: &str, font_size: f32) -> Result<(f32, f32), TextSizeError> {
-//         let font_str = format!("{}px {}", font_size, font_family);
-//         let x = get_text_width(text, &font_str);
-//         let y = get_text_height(text, &font_str);
-//         Ok((x,y))
-//     }
-// }
+impl text_size::TextSizer for WASMTextSizer {
+    fn text_size(&self, text: &str, font_family: &str, font_size: f32) -> Result<(f32, f32), text_size::TextSizeError> {
+        let font_str = format!("{}px {}", font_size, font_family);
+        let x = get_text_width(text, &font_str);
+        let y = get_text_height(text, &font_str);
+        Ok((x,y))
+    }
+}
 
 
 
 /// This must be called first, to initialize things in the rust world.
 #[wasm_bindgen]
 pub fn initialize() {
-    // unsafe { // must be called before anything else happens, then never called again
-    //     text_size::set_system_text_sizer(&WASMTextSizer);
-    // }
+    unsafe { // must be called before anything else happens, then never called again
+        text_size::set_system_text_sizer(&WASMTextSizer);
+    }
 }
 
 
 #[wasm_bindgen]
 pub fn get_svg() -> String {
-    // match global_document().lock().unwrap().get_svg_str() {
-    //     Ok(s) => s.into(),
-    //     Err(_) => "<h1>Error</h1>".into(),
-    // }
-    "<h1>Error</h1>".into() // FIXME: Remove
+    match global_document().lock().unwrap().get_svg_str() {
+        Ok(s) => s.into(),
+        Err(_) => "<h1>Error</h1>".into(),
+    }
 }
 
 #[wasm_bindgen]
-pub fn toggle_node(_node_id: u32) -> String {
-    // global_document().lock().unwrap().toggle_collapse(node_id.try_into().unwrap());
+pub fn toggle_node(node_id: u32) -> String {
+    global_document().lock().unwrap().toggle_collapse(node_id.try_into().unwrap());
     get_svg()
 }
 
@@ -99,3 +99,12 @@ fn global_document<'a>() -> &'a Mutex<TwoTreeViewDocument> {
     });
     unsafe { GLOBAL_DOCUMENT.as_ref().unwrap() }
 }
+
+//
+// FIXME:
+//   Folks on the Rust discord who helped me with another issue suggested that instead of doing
+//   GLOBAL_DOCUMENT as a "static mut" I should use either once_cell::sync::Lazy or else
+//   use "static GLOBAL_DOCUMENT: Mutex<Option<TwoTreeViewDocument>> = Mutex::new(None)"
+//   |
+//   I should try it
+//
